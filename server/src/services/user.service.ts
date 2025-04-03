d411a89f113839dc50d943c77285e685c700fa43
ritesh-metagame <ritesh@meta-game.ai>
2025-04-03 16:09:46 +0530

    Add affiliate link to User model and update user creation logic

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,20 +1,29 @@
-import { Service } from "typedi";
+import Container, { Service } from "typedi";
 import UserDao from "../daos/user.dao";
 import { RoleDao } from "../daos/role.dao";
-import { Role, User } from "../../prisma/generated/prisma";
+import { Commission, Role, User } from "../../prisma/generated/prisma";
 import { UserRole } from "../common/config/constants";
 import { Response } from "../common/config/response";
 import { ResponseCodes } from "../common/config/responseCodes";
 import { BcryptService } from "../common/lib";
+import { CommissionService } from "./commission.service";
+import { CategoryService } from "./category.service";
+import { SiteService } from "./site.service";
 
 @Service()
 class UserService {
   private userDao: UserDao;
   private roleDao: RoleDao;
+  private commissionService: CommissionService;
+  private categoryService: CategoryService;
+  private siteService: SiteService;
 
   constructor() {
     this.userDao = new UserDao();
     this.roleDao = new RoleDao();
+    this.commissionService = Container.get(CommissionService);
+    this.categoryService = Container.get(CategoryService);
+    this.siteService = Container.get(SiteService);
   }
 
   public async createUser(userData: Partial<User>, roleId: string, user: User) {
@@ -53,6 +62,47 @@ class UserService {
       };
 
       const newUser = await this.userDao.createUser(data);
+
+      const sites = await this.siteService.getAllSites(
+        user,
+        currentUserRole.name
+      );
+
+      const userSite = await this.siteService.createUserSite({
+        userId: newUser.id,
+        siteId: sites.data[Math.floor(Math.random() * sites.data.length)].id,
+      });
+
+      const roles = await this.roleDao.getAllRolesForSuperAdmin();
+
+      const requiredRoles = roles.filter(
+        (r) => r.name !== UserRole.SUPER_ADMIN
+      );
+
+      console.log({ requiredRoles });
+
+      const categories = await this.categoryService.getAllCategories();
+
+      for (const r of requiredRoles) {
+        for (const category of categories.data) {
+          let commissionPercentage = 2.0;
+          if (r.name === UserRole.PLATINUM) {
+            commissionPercentage = 3.0; // Example percentage for Platinum
+          } else if (r.name === UserRole.GOLDEN) {
+            commissionPercentage = 2.0; // Example percentage for Golden
+          } else if (r.name === UserRole.OPERATOR) {
+            commissionPercentage = 5.0; // Example percentage for Operator
+          }
+          const commission: Partial<Commission> = {
+            userId: newUser.id,
+            roleId: r.id,
+            categoryId: category.id,
+            siteId: sites.data[0].id, // Default percentage
+            commissionPercentage: commissionPercentage,
+          };
+          await this.commissionService.createCommission(commission);
+        }
+      }
 
       return new Response(
         ResponseCodes.USER_CREATED_SUCCESSFULLY.code,
