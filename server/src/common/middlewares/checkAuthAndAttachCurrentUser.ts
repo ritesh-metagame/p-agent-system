1ec20c9daa4603fcb03487946b4f501a06773879
Abiksoumya <soumyamohanty540@gmail.com>
2025-04-04 01:47:59 +0530

    update log entries and modify excluded paths in authentication middleware

--- server/src/common/middlewares/checkAuthAndAttachCurrentUser.ts
+++ server/src/common/middlewares/checkAuthAndAttachCurrentUser.ts
@@ -12,8 +12,8 @@ import UserDao from "../../daos/user.dao";
 const log = getLogger(module);
 
 const excludedPathsFromMiddleware = [
-  "/api/v1/auth/login",
-  "/api/v1/category",
+  "/v1/auth/login",
+  // "/v1/category",
   "/",
 ];
 
