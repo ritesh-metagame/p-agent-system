782150bab2ce4727d962677716999a678db4c565
ritesh-metagame <ritesh@meta-game.ai>
2025-05-23 18:56:53 +0530

    fix(logs): add duplicate log entries for server startup and express loading fix(commission): adjust cycle end date calculation for accurate commission reporting

--- server/src/services/commission.service.ts
+++ server/src/services/commission.service.ts
@@ -3337,21 +3337,21 @@ console.log({commissionSummaries})
           currentDate.getMonth(),
           1
         );
-        cycleEndDate = new Date(
+        cycleEndDate = new Date(new Date(
           currentDate.getFullYear(),
           currentDate.getMonth(),
           15
-        );
+        ).setHours(23, 59, 59, 999))
       } else {
         // We're in first half, show second half of previous month
-        cycleStartDate = new Date(
+        cycleStartDate = new Date(new Date(
           currentDate.getFullYear(),
           currentDate.getMonth() - 1,
           16
-        );
-        cycleEndDate = endOfMonth(
+        ).setHours(0, 0, 0, 0));
+        cycleEndDate = new Date(endOfMonth(
           new Date(currentDate.getFullYear(), currentDate.getMonth() - 1)
-        );
+        ).setHours(23, 59, 59, 999));
       }
     }
 
@@ -3378,7 +3378,7 @@ console.log({commissionSummaries})
 
     return {
       cycleStartDate: startOfPrevWeek,
-      cycleEndDate: mostRecentSunday,
+      cycleEndDate: new Date(mostRecentSunday.setHours(23, 59, 59, 999)),
     };
   }
 
